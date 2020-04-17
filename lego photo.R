e736ca28a8aa1c0a72100a6a90199d860437932f
WONGY66 <59711659+WONGY66@users.noreply.github.com>
2020-04-17 21:33:09 +0800

    Update lego photo.R

--- lego photo.R
+++ lego photo.R
@@ -18,7 +18,7 @@ mosaic %>% build_mosaic()
 
 mosaic <- png::readPNG("~/xmen.png") %>% 
   image_to_mosaic(contrast = 1.1,
-                  use_bricks = c("4x6", "2x4", "1x2", "1x1")) %>% bricks_from_mosaic()
+                  use_bricks = c("4x6", "2x4", "1x2", "1x1")) %>% bricks_from_mosaic(highest_el = "dark")
 
 mosaic %>% build_bricks()
 
@@ -27,7 +27,7 @@ mosaic %>% build_bricks()
 ironman <- png::readPNG("~/ironman.png") %>% 
   image_to_mosaic(brightness = 1.1,
                   contrast = 1.1,
-    use_bricks = c("4x6", "2x4", "1x2", "1x1")) %>% bricks_from_mosaic()
+    use_bricks = c("4x6", "2x4", "1x2", "1x1")) %>% bricks_from_mosaic(highest_el = "dark")
 
 ironman %>% build_bricks()
 
